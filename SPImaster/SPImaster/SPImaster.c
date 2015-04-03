057fedc27d686332fb2aef1eb8862f53419965b3
Frida Sundberg <sundberg.fr@gmail.com>
2015-04-03 11:27:28 +0200

    Partly working communication with two slaves

--- SPImaster/SPImaster/SPImaster.c
+++ SPImaster/SPImaster/SPImaster.c
@@ -1,7 +1,7 @@
 /*
  * Test1masterDatabuss.c
  *
- * Created: 31-03-2015
+ * Created: 02-04-2015
  * Author: Frida Sundberg, Markus Petersson
  * 
  */ 
@@ -9,73 +9,253 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include "buffer.h"
 #define F_CPU = 16000000UL
 
-volatile int slave_ready = 1;
+volatile int slave1_ready = 1;
+volatile int slave2_ready = 1;
 volatile int failed_attempts = 0;
+volatile int transmission_status = 0; // 0 to send .type, 1 to send .val, 2 when both sent
+volatile int counter = 0;
+volatile struct data_byte temp_data;
+
+struct data_buffer slave1_buffer;
+struct data_buffer slave2_buffer;
+struct data_buffer pc_buffer;
 
 void init_master(void)
 {
 	// SCK=f/16
 	SPCR = (1<<SPIE)|(1<<SPE)|(0<<DORD)|(1<<MSTR)|(0<<CPOL)|(0<<CPHA)|(0<<SPR1)|(0<<SPR0);
 	SPSR = (0<<SPI2X);
-	DDRB = (1<<DDB7)|(1<<DDB5)|(1<<DDB4);
-	PORTB = (1<<PORTB4); // Pulling SS2 high
+	DDRB = (1<<DDB7)|(1<<DDB5)|(1<<DDB4)|(1<<DDB3)|(1<<DDB0);
+	PORTB = (1<<PORTB4)|(1<<PORTB3); // Pulling SS2 and SS1 high
 	
+	// PC0 output, PD7 output (ext int to slave 2 to change receive/send-variable in slave 2)
+	DDRC = (1<<DDC0);
+	DDRD = (1<<DDD7)|(1<<DDD6);
+	
+	// IRQ1 and IRQ0 activated on rising edge
+	EICRA = (1<<ISC11)|(1<<ISC10)|(1<<ISC01)|(1<<ISC00);
+	// Enable IRQ1 and IRQ0
+	EIMSK = (1<<INT1)|(1<<INT0);
 	// IRQ0 activated on rising edge
-	EICRA = (1<<ISC01)|(1<<ISC00);
-	// Enable IRQ0
-	EIMSK = (1<<INT0);
+	
+	// Init data buffers in master
+	buffer_init(&slave1_buffer);
+	buffer_init(&slave2_buffer);
+	buffer_init(&pc_buffer);
 	
 	// Let PA be outputs for testing
 	DDRA = 0xFF;
 };
 
-void send_to_slave2(volatile char send_data)
+void send_to_slave1()
 {
-	slave_ready = 0;
-	PORTB = (0<<PORTB4); // Pulling SS2 low
-	SPDR = send_data;
-	_delay_us(20);
+	send(&slave1_buffer, 1);
 };
 
-/*
-void receive_from_slave2(void)
+void send_to_slave2()
 {
-	send_to_slave2(0xAA);	
-};*/
+	send(&slave2_buffer, 2);
+};
+
+void send(struct data_buffer* my_buffer, int slave)
+{
+	int *current_slave_ready;
+	if (slave == 1)
+	{
+		current_slave_ready = &slave1_ready;
+		PORTB = (0<<PORTB0); // Order slave 1 to adapt receive mode
+		PORTB = (1<<PORTB0);
+	}
+	else if(slave == 2)
+	{
+		current_slave_ready = &slave2_ready;
+		PORTC = (0<<PORTC0); // Order slave 2 to adapt receive mode
+		PORTC = (1<<PORTC0);	
+	}
+	struct data_byte send_byte = fetch_from_buffer(my_buffer); // Fetch one byte from buffer without discarding
+	transmission_status = 0; // Integer to indicate how far the send process has proceeded, updated when acknowledgment received from slave (in external interrupt) 
+	volatile int local_failed_attempts = 0; 
+	while(1)
+	{
+		if (slave == 1)
+		{
+			PORTB = (0<<PORTB3); // Pulling SS1 low
+		}
+		else if(slave==2)
+		{
+			PORTB = (0<<PORTB4); // Pulling SS2 low	
+		}
+		if(*current_slave_ready==1)
+		{
+			if(transmission_status == 0) // Ready to send .type part
+			{
+				*current_slave_ready = 0;
+				SPDR = send_byte.type;
+				_delay_us(30);
+			}
+			else if(transmission_status == 1) // Ready to send .val part
+			{
+				*current_slave_ready = 0;
+				SPDR = send_byte.val;
+				_delay_us(30);	
+			}
+			else if(transmission_status == 2) // Full send_byte transmitted correctly
+			{
+				discard_from_buffer(my_buffer); // Discard byte from buffer when full transmission succeeded
+				counter++;
+				PORTA = counter;
+				transmission_status = 0;
+				break;
+			}
+		}
+		else
+		{
+			failed_attempts++;
+			local_failed_attempts++;
+			if(local_failed_attempts > 10000) // Ten thousand tries allowed before leaving function
+			{
+				transmission_status = 0;
+				break;
+			}
+		}
+	}
+	
+};
+
+void receive_from_slave1()
+{
+	receive(1);
+};
+
+void receive_from_slave2()
+{
+	receive(2);
+};
+
+void receive(int slave)
+{
+	int local_failed_attempts = 0;
+	int *current_slave_ready;
+	if (slave == 1)
+	{
+		current_slave_ready = &slave1_ready;
+		PORTD = (0<<PORTD6); // Order slave 1 to adapt send mode
+		PORTD = (1<<PORTD6);
+	}
+	else if(slave == 2)
+	{
+		current_slave_ready = &slave2_ready;
+		PORTD = (0<<PORTD7); // Order slave 2 to adapt send mode
+		PORTD = (1<<PORTD7);
+	}
+	transmission_status=0;
+	counter=0;
+	while(1)
+	{
+		//Select the right slave.
+		if (slave == 1)
+		{
+			PORTB = (0<<PORTB3); // Pulling SS1 low
+		}
+		else if(slave == 2)
+		{
+			PORTB = (0<<PORTB4); // Pulling SS2 low	
+		}
+		//check if the current slave is ready to send. The slave_ready status is set through acknowledgments.
+		if(*current_slave_ready == 1)
+		{
+			//if statement to check where in the transmission process we are currently at.
+			if(transmission_status == 0)
+			{
+				*current_slave_ready = 0;
+				SPDR = 0x00;//random data to send to slave, since the slave is sending it does not care what it gets.
+				_delay_us(30);
+			}
+			else if(transmission_status == 1)
+			{
+				temp_data.type = SPDR;//read the data that the slave has sent. Note that SPDR should be read here and not where transmission_status==0 
+									  //to make sure that we have gotten an acknowledgment from the slave
+				*current_slave_ready = 0;
+				SPDR = 0x00;//random data to send to slave, since the slave is sending it does not care what it gets.
+				_delay_us(30);
+				
+			}
+			else if(transmission_status==2)
+			{
+				temp_data.val = SPDR; //read the data that the slave has sent.
+				transmission_status = 0;
+				if(temp_data.type == 0 && temp_data.val == 0) //if we have received a null_data_byte: ignore.
+				{
+					
+				}
+				else
+				{
+					if (slave == 1)
+					{
+						add_to_buffer(&slave2_buffer, temp_data.type, temp_data.val);
+					}
+					_delay_us(30);
+					add_to_buffer(&pc_buffer, temp_data.type, temp_data.val); //otherwise add to buffer!	
+				}
+				_delay_us(30);
+				break;
+				
+			}
+		}
+		else //Safety net so that we  don't get stuck in a never ending loop if something with the transmission goes to... hell.
+		{
+			local_failed_attempts++;
+			if(local_failed_attempts > 10000) // Ten thousand tries allowed before leaving function
+			{
+				transmission_status = 0;
+				break;
+			}
+		}
+	}
+};
+
+//////////////////////////////////////////////////////////////////////
+//----------------------------  MAIN -------------------------------//
+//////////////////////////////////////////////////////////////////////
 
 int main(void)
 {
 	init_master();
 	sei();
 	
-	for (int i=0; i<255; i++)
+	
+	for(int i=1; i<64; i++)
 	{
-		if(slave_ready==1)
-		{
-			send_to_slave2(i);	
-		}
-		else
-		{
-			failed_attempts++;
-		}
+		add_to_buffer(&slave1_buffer,i,i);
 	}
 	
-	PORTA = failed_attempts;
+	while(buffer_empty(&slave1_buffer) == 0)
+	{	
+		send_to_slave1();
+	}
 		
 	while(1)
     {
-       
+		
     }
 }
 
 ISR(INT0_vect)
 {
-	slave_ready = 1;
+	slave2_ready = 1;
+	transmission_status++;
+}
+
+ISR(INT1_vect)
+{
+	slave1_ready = 1;
+	transmission_status++;
 }
 
 ISR(SPI_STC_vect)
 {	
-	PORTB = (1<<PORTB4); // Pulling SS2 high
+	PORTB = (1<<PORTB4)|(1<<PORTB3); // Pulling SS2 and SS1 high
 }
--- SPIslave1/SPIslave1/SPIslave1.c
+++ SPIslave1/SPIslave1/SPIslave1.c
@@ -1,5 +1,5 @@
 /*
- * Test1slave2Databuss.c
+ * SPIslave1.c
  *
  * Created: 02-04-2015
  * Author: Frida Sundberg, Markus Petersson
@@ -17,7 +17,7 @@ volatile int transmission_status = 0;
 volatile struct data_byte temp_data;
 volatile int counter = 0;
 
-void init_slave2(void)
+void init_slave1(void)
 {
 	SPCR = (1<<SPIE)|(1<<SPE)|(0<<DORD)|(0<<MSTR)|(0<<CPOL)|(0<<CPHA);
 	DDRB = (1<<DDB6)|(1<<DDB3);
@@ -34,51 +34,43 @@ void init_slave2(void)
 	//Initiate the buffers.
 	buffer_init(&receive_buffer);
 	buffer_init(&send_buffer);
-	
 };
-
-// todo: functions for transmit and receive
 
 void send_to_master(struct data_buffer* my_buffer)
 {
-		//if Transmission not yet started: fetch type and put it in SPDR.
-		if(transmission_status==0)
+
+		if(transmission_status == 0)
 		{
 			SPDR = fetch_from_buffer(my_buffer).type;
 		}
-		//if Type already sent: fetch val and put it in SPDR.
-		else if(transmission_status==1)
+		else if(transmission_status == 1)
 		{
 			SPDR = fetch_from_buffer(my_buffer).val;
 		}
-		//if the master has accepted both bytes that were sent: discard the data_byte from the buffer.
-		else if(transmission_status==2)
+		else if(transmission_status == 2)
 		{
 			discard_from_buffer(my_buffer);
-			transmission_status=0;
+			transmission_status = 0;
 		}
 	
 	
 };
 
-//Method to receive a data_byte. Called when SPI interrupt has occurred.
 void receive_data(struct data_buffer* my_buffer)
 {
-	//get type.
 	if(transmission_status == 0)
 	{
 		temp_data.type = SPDR;
 	}
-	//get val.
 	else if(transmission_status == 1)
 	{
 		counter++;
 		temp_data.val = SPDR;
-		add_to_buffer(my_buffer, temp_data.type, temp_data.val);//add to receive_buffer when	
+		add_to_buffer(my_buffer, temp_data.type, temp_data.val);	
 		transmission_status = 0;
 		PORTA = counter;	
 	}
-}
+};
 
 //////////////////////////////////////////////////////////////////////
 //----------------------------  MAIN -------------------------------//
@@ -86,24 +78,27 @@ void receive_data(struct data_buffer* my_buffer)
 
 int main(void)
 {
-	init_slave2();
+	init_slave1();
 	sei();
-	/*add_to_buffer(&send_buffer, 0xAA, 0xAA);
+	/*
+	add_to_buffer(&send_buffer, 0xAA, 0xAA);
 	add_to_buffer(&send_buffer, 0xAA, 0xAA);
 	add_to_buffer(&send_buffer, 0xAA, 0xAA);
 	add_to_buffer(&send_buffer, 0xAA, 0xAA);
 	add_to_buffer(&send_buffer, 0xAA, 0xAA);*/
+	
     while(1)
     {
-		//PORTA = amount_stored(&receive_buffer);
+		// PORTA = amount_stored(&receive_buffer);
+		// PORTA = amount_stored(&send_buffer);
 	}
 }
 
 ISR(SPI_STC_vect)
 {
 	PORTB = (0<<PORTB3);
-	PORTB = (1<<PORTB3);
-	//Depending on the current mode: do things.		
+	PORTB = (1<<PORTB3);		
+	
 	if(mode == 0)
 	{
 		receive_data(&receive_buffer);
