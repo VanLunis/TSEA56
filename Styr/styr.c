82c568be2cdea2435bc1979b90a43dc72440ae2c
Ola Grankvist <ola.grankvist@gmail.com>
2015-04-16 11:34:34 +0200

    updated with new ouput pins q

--- Styr/styr.c
+++ Styr/styr.c
@@ -1,21 +1,14 @@
-//
-//  styr.c
-//  
-//  Created by Ola Grankvist on 30/03/15.
-
-
-#include <stdio.h>
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
-//#define F_CPU 1000000
+//#define F_CPU 8000000UL
 
 #define DD_SS DDB4
 #define DD_MOSI DDB5
 #define DD_MISO DDB6
 #define DD_SCK DDB7
-#define FULL_SPEED 50
-#define SLOW_SPEED 25
+#define FULL_SPEED 25
+#define SLOW_SPEED 10
 
 // limits for motor control signals:   VERY_NEGATIVE_LIMIT < SLIGHT_NEGATIVE_LIMIT < SLIGHT_POSITIVE_LIMIT < VERY_POSITIVE_LIMIT
 #define VERY_NEGATIVE_LIMIT -30
@@ -23,13 +16,14 @@
 #define SLIGHT_POSITIVE_LIMIT 10
 #define VERY_POSITIVE_LIMIT 30
 
+#define AUTONOMOUS_MODE 1
+#define REMOTE_CONTROL_MODE 0
 
 /*
  A = left wheels
  B = right wheels
  
  */
-
 
 void init();
 void pwm_init();
@@ -44,6 +38,7 @@ void slight_right();
 void sharp_left();
 void sharp_right();
 void backward();
+void stopp();
 double controller(int e, int alpha, int e_prior, int alpha_prior);
 void setMotor(double u);
 void grip_object();
@@ -56,51 +51,41 @@ void move_arm();
 
 int main(void)
 {
+    
     // initialize:
-    // sei(); inte?
     init();
     
-    double alpha = 0;
-    double local_alpha;
-    double e = 0;
-    double local_e;
-    double e_prior = 0;
-    double alpha_prior = 0;
-    
-    double u;
-    
-    
-    DDRC = (1<< DDC7);
-    
-    
-    
-    while(1)
+    for (;;)
     {
         local_e = e;
-        local_alpha = alpha;
+        local_alpha = alpha;;
         u = controller(local_e, local_alpha, e_prior, alpha_prior);
         setMotor(u);
         e_prior = local_e;
         alpha_prior = local_alpha;
-        
-        
-        PORTC = (1 << PORTC7);
-        _delay_ms(1);
-        PORTC = (0 << PORTC7);
-        _delay_ms(20);
     }
+    
+    
 }
 
 
-/*_____________________________________________________________________
+/*
+ _______________________________________________________________________
  ______________________________ FUNCTIONS ______________________________
- _______________________________________________________________________ */
+ _______________________________________________________________________
+ */
+
+
+// _______________________ INITILIZES FUNCTIONS ________________________
+
 void init(){
     pwm_init();
     SPI_slave_init();
 }
-void pwm_init()
-{
+
+void pwm_init(){
+    
+    DDRD |= (1<<DDD6)|(1<<DDD7);		// sets OCR2A and OCR2B as outputs => needed?
     OCR2A = 0;
     OCR2B = 0;
     
@@ -115,24 +100,20 @@ void pwm_init()
     OCR2A = 0; // init compare, i.e. init speed=0
     OCR2B = 0; // init compare, i.e. init speed=0
     
-    // set port 20 and port 21 as right resp. left
-    DDRD = 0xFF;
+    // set port 22 and port 23 as right resp. left
+    DDRC = (1 << DDC0) | (1 << DDC1);
     // Initiate gear as 11: straight forward
-    PORTD = (1<<PORTD0) | (1<<PORTD2);
+    PORTC = (1<<PORTC1) | (1<<PORTC0);
 }
 void SPI_slave_init(){
     DDRB = (1 << DD_MOSI) | (0 << DD_SS) | (0 << DD_MISO)| (0 << DD_SCK); // sets output: MIS0; input: SS, MOSI, SCK
     SPCR = (1 << SPE) | (1 << SPIE); // SPIE enables SPI interrupts
 }
 
+
 // _______________________ INTERRUPTS FUNCTIONS ________________________
 
-ISR(SPI_STC_vect){
-    PORTA = SPDR;
-    
-    
-    
-}
+
 
 
 // ________________________ CONTROL FUNCTIONS __________________________
@@ -180,42 +161,47 @@ void set_speed_left_wheels(unsigned char new_speed_percentage){
 
 // Steering functions
 void forward(){
-    PORTD = (1<<PORTD0) | (1<<PORTD2);
+    PORTC = (1<<PORTC1) | (1<<PORTC0);
     set_speed_right_wheels(FULL_SPEED);
     set_speed_left_wheels(FULL_SPEED);
 }
 void rotate_left(){
-    PORTD = (0<<PORTD0) | (1<<PORTD2); // sets right forward, left backward
+    PORTC = (0<<PORTC1) | (1<<PORTC0); // sets right forward, left backward
     set_speed_right_wheels(FULL_SPEED);
     set_speed_left_wheels(FULL_SPEED);
 }
 void rotate_right(){
-    PORTD = (1<<PORTD0) | (0<PORTD2); // sets left forward, right backward
+    PORTC = (1<<PORTC1) | (0<PORTC0); // sets left forward, right backward
     set_speed_right_wheels(FULL_SPEED);
     set_speed_left_wheels(FULL_SPEED);
 }
 void slight_left(){
-    PORTD = (1<<PORTD0) | (1<<PORTD2);
+    PORTC = (1<<PORTC1) | (1<<PORTC0);
     set_speed_right_wheels(FULL_SPEED);
     set_speed_left_wheels(SLOW_SPEED);
 }
 void slight_right(){
-    PORTD = (1<<PORTD0) | (1<<PORTD2);
+    PORTC = (1<<PORTC1) | (1<<PORTC0);
     set_speed_right_wheels(SLOW_SPEED);
     set_speed_left_wheels(FULL_SPEED);
 }
 void sharp_left(){
-    PORTD = (1<<PORTD0) | (1<<PORTD2);
+    PORTC = (1<<PORTC1) | (1<<PORTC0);
     set_speed_right_wheels(FULL_SPEED);
     set_speed_left_wheels(0);
 }
 void sharp_right(){
-    PORTD = (1<<PORTD0) | (1<<PORTD2);
+    PORTC = (1<<PORTC1) | (1<<PORTC0);
     set_speed_right_wheels(0);
     set_speed_left_wheels(FULL_SPEED);
 }
 void backward(){
-    PORTD = (0<<PORTD0) | (0<<PORTD2);
+    PORTC = (0<<PORTC1) | (0<<PORTC0);
     set_speed_right_wheels(FULL_SPEED);
     set_speed_left_wheels(FULL_SPEED);
+}
+void stopp(){
+    PORTC = (1<<PORTC1) | (1<<PORTC0);
+    set_speed_right_wheels(0);
+    set_speed_left_wheels(0);
 }
